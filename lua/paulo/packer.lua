72cf0c120edaee485dc04241a5652cea960c31d4
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-10-28 13:05:29 -0300

    feat: Update plugins
    - Identation
    - Remove cokeline, change to use lualine tabs
    - Added obsession, prosession to save and load on open/exit nvim
    - Added tabscope to control buffers by tab
    - Added gitsigns to show changes in line

--- lua/paulo/packer.lua
+++ lua/paulo/packer.lua
@@ -4,51 +4,49 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.4',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = {'nvim-lua/plenary.nvim'}
   }
   use{ 'rose-pine/neovim', as = 'rose-pine' }
-  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use( 'nvim-treesitter/playground')
-  use( 'mbbill/undotree')
-  use( 'tpope/vim-fugitive')
-  use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  use{ "catppuccin/nvim", as = "catpuccin" } 
+  use{ 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} }
+  use{ 'nvim-treesitter/playground' }
+  use{ 'mbbill/undotree' }
+  use{ 'tpope/vim-fugitive' }
+  use{ 'tpope/vim-surround' }
+  use{ 'tpope/vim-obsession' }
+  use{ 'dhruvasagar/vim-prosession' }
+  use{ 'backdround/tabscope.nvim' }
+  use{ 'lewis6991/gitsigns.nvim' }
+  use{
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }
-  use {
-	  "willothy/nvim-cokeline",
-	  dependencies = {
-		  "nvim-lua/plenary.nvim",        -- Required for v0.4.0+
-		  "kyazdani42/nvim-web-devicons", -- If you want devicons
-		  "stevearc/resession.nvim"       -- Optional, for persistent history
-	  },
-	  config = true
-  }
-  use{ "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
+  use{
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     requires = { 
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
       {
         's1n7ax/nvim-window-picker',
         version = '2.*',
         config = function()
-            require 'window-picker'.setup({
-                filter_rules = {
-                    include_current_win = false,
-                    autoselect_one = true,
-                    -- filter using buffer options
-                    bo = {
-                        -- if the file type is one of following, the window will be ignored
-                        filetype = { 'neo-tree', "neo-tree-popup", "notify" },
-                        -- if the buffer type is one of following, the window will be ignored
-                        buftype = { 'terminal', "quickfix" },
-                    },
+          require 'window-picker'.setup({
+            filter_rules = {
+              include_current_win = false,
+              autoselect_one = true,
+              -- filter using buffer options
+              bo = {
+                -- if the file type is one of following, the window will be ignored
+                filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+                -- if the buffer type is one of following, the window will be ignored
+                buftype = { 'terminal', 'quickfix' },
+              },
             },
-        })
+          })
         end,
       },
     },
-}
+  }
 end)
