eb934a1e272b26c9ef88c26697d3bf34aed001f9
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-10-28 12:11:36 -0300

    feat: Included to show tabs in lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -33,7 +33,14 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
+  tabline = {
+    lualine_a = {'buffers'},
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {'tabs'}
+  },
   winbar = {},
   inactive_winbar = {},
   extensions = {}
