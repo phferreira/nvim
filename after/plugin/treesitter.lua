138c37c41ec5dea02005243d7d7eea21b4ab1eda
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-09 18:19:35 -0300

    feat: Update treesitter configs
    - Set indent to false, on indent xml this property indent wrong
    - Disable highlight, xml not indent with this
    - Add more languages

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,12 +1,46 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "javascript", "typescript", "dart",  "c", "lua", "vim", "vimdoc", "query" },
+ require'nvim-treesitter.configs'.setup {
+  ensure_installed = {
+    'javascript',
+    'typescript',
+    'dart',
+    'c',
+    'lua',
+    'vim',
+    'vimdoc',
+    'query',
+    'html',
+    'sql',
+    'yaml',
+    'vim',
+    'tsx',
+    'regex',
+    'json',
+    'bash'
+  },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd= false
+  },
   sync_install = false,
   auto_install = true,
   highlight = {
-    enable = true,
+    enable = false,
+    disable = function (_, bufnr) return vim.b[bufnr].large_buf end,
     additional_vim_regex_highlighting = false,
   },
-  indent = {
+  incremental_selection = {
     enable = true
-  }
+  },
+  indent = {
+    enable = false
+  },
+  autotag = {
+    enable = true,
+    filetypes = {
+      "html",
+      "javascript",
+      "typescript",
+      "xml",
+    },
+  },
 }
