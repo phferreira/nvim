16d1754013af4fc8f0b4e645bb143acf4c70dfd0
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-14 19:40:27 -0300

    feat: Remove maps from lsp
    - They pass to use in completions.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,22 +1,8 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.omnifunc.setup({
-  autocomplete = true,
-  use_fallback = true,
-  update_on_delete = true,
-  trigger = '<C-Space>',
-  select_behavior = 'insert',
-  mapping = {
-    prev_item = '<S-TAB>',
-    next_item = '<TAB>'
-  }
-})
-
 lsp_zero.on_attach(function(_, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr} )
 end)
-
-
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
