89dd838a9b1abe97707a7001a2cb5b3ba8ca47b9
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-17 16:07:24 -0300

    style: Set highlight colors for git diff

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,11 @@
 function ColorMyPencils(color)
 	color = color or 'rose-pine'
 	vim.cmd.colorscheme(color)
+
+  vim.cmd('hi DiffAdd    gui=NONE      guifg=#000000 guibg=#3E50D8')
+  vim.cmd('hi DiffChange gui=NONE      guifg=#135B04 guibg=#51D137')
+  vim.cmd('hi DiffDelete gui=NONE      guifg=#000000 guibg=#f25757')
+  vim.cmd('hi DiffText   gui=NONE      guifg=#000000 guibg=#B9F1AD')
 end
 
 ColorMyPencils('catppuccin')
