898b8464d301c28fb0d956798b44a5dd9f1cbf71
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-18 13:33:19 -0300

    feat: Added LSP format on save

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,18 @@ lsp_zero.on_attach(function(_, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr} )
 end)
 
+lsp_zero.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['tsserver'] = {'javascript', 'typescript'},
+    ['rust_analyzer'] = {'rust'},
+    ['dartls'] = {'dart'}
+  }
+})
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
