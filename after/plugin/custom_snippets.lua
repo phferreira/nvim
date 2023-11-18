12fdb2a899fd43c2720c1b852df9814fcb7fde1e
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-17 22:48:34 -0300

    refactor: Change " for '

--- after/plugin/custom_snippets.lua
+++ after/plugin/custom_snippets.lua
@@ -4,8 +4,8 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-ls.add_snippets("gitcommit", {
-	s("tar", {
+ls.add_snippets('gitcommit', {
+	s('tar', {
 		t('Tarefa: #'), i(1), t(" - "), i(2)
 	})
 })
