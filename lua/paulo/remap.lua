032054bae6e9da01b99e83dbba03554b2ac86135
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-18 18:29:18 -0300

    refactor: Detach remaps by context

--- lua/paulo/remap.lua
+++ lua/paulo/remap.lua
@@ -1,22 +1,32 @@
 vim.g.mapleader = ' '
 
+-- MOVE SELCTED LINES
 vim.keymap.set('v', '<C-K>', ':m \'<-2<CR>gv=gv')
 vim.keymap.set('v', '<C-J>', ':m \'>+1<CR>gv=gv')
+
+-- KEEP SELECTION
 vim.keymap.set('v', '>',     '>gv')
 vim.keymap.set('v', '<',     '<gv')
 vim.keymap.set('v', 'u',     'ugv')
 vim.keymap.set('v', 'U',     'Ugv')
 
+-- CENTER ON COMMAND
 vim.keymap.set('n', '<space><space>', 'i<space><ESC>')
 vim.keymap.set('n', 'n',              'nzzzv')
 vim.keymap.set('n', 'N',              'Nzzzv')
 vim.keymap.set('n', 'J',              'mzJ`z')
+vim.keymap.set('n', '<C-d>',          '<C-d>zz')
+vim.keymap.set('n', '<C-u>',          '<C-u>zz')
+
+-- FINDS
 vim.keymap.set('n', '[q',             ':cprev<CR>')
 vim.keymap.set('n', ']q',             ':cnext<CR>')
 vim.keymap.set('n', '[Q',             ':cfirst<CR>')
 vim.keymap.set('n', ']Q',             ':clast<CR>')
 vim.keymap.set('n', '[<C-Q>',         ':cpfile<CR>')
 vim.keymap.set('n', ']<C-Q>',         ':cnfile<CR>')
+
+-- NAVIGATION
 vim.keymap.set('n', '<M-l>',          ':bn<cr>')
 vim.keymap.set('n', '<M-h>',          ':bp<cr>')
 vim.keymap.set('n', '<C-M-l>',        ':tabnext<cr>')
@@ -24,12 +34,14 @@ vim.keymap.set('n', '<C-M-h>',        ':tabprevious<cr>')
 vim.keymap.set('n', '<C-M-k>',        ':tabnew<cr>')
 vim.keymap.set('n', '<C-M-j>',        ':tabclose<cr>')
 
+-- CLOSE BUFFER
+vim.keymap.set('n', '<leader>c',  require("tabscope").remove_tab_buffer)
+
+-- INSERT LINE 
 vim.keymap.set('n', '<C-j>', 'i<CR><ESC>')
 vim.keymap.set('n', '<C-k>', 'O<ESC>')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
-vim.keymap.set('n', '<leader>c',  require("tabscope").remove_tab_buffer)
+-- REPLACE
 vim.keymap.set('n', '<leader>r',  ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/g<LEFT><LEFT>')
 vim.keymap.set('n', '<leader>rc', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gc<LEFT><LEFT><LEFT>')
 
@@ -40,6 +52,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
+-- DISABLE ARROW KEYS
 vim.keymap.set('n', '<UP>',    '<NOP>')
 vim.keymap.set('n', '<DOWN>',  '<NOP>')
 vim.keymap.set('n', '<LEFT>',  '<NOP>')
@@ -63,6 +76,7 @@ vim.keymap.set('n', '<leader>fa' , ':lua FlutterAnalyze()<cr>', { noremap = true
 vim.keymap.set('n', '<leader>ft' , ':lua FlutterTest()<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fct', ':lua FlutterCreateTest()<cr>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fta', ':lua FlutterTestAll()<cr>', { noremap = true, silent = true })
+
 -- CODE ACTION
 vim.keymap.set('n', '<M-CR>', require("actions-preview").code_actions, { noremap = true, silent = true })
 
@@ -86,3 +100,6 @@ vim.keymap.set('n', '<leader>gC', ':lua require\'telescope.builtin\'.git_branche
 vim.keymap.set('n', '<leader>gS', ':lua require\'telescope.builtin\'.git_stash()<CR>')
 vim.keymap.set('n', '<leader>gL', ':lua require\'telescope.builtin\'.git_commits()<CR>')
 vim.keymap.set('n', '<leader>d', ':lua ToggleDiagnostics()<CR>')
+
+-- SHOW TODOS
+vim.keymap.set('n', '<leader>td', ':TodoTelescope<CR>')
