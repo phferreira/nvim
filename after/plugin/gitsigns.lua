c3b97cd877c3e02bb6a711f239d6945e196c2ca5
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-20 16:59:43 -0300

    docs: Indentation

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -11,20 +11,20 @@ gitsigns.setup {
     topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
     changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
   },
-  signcolumn = true,   -- Toggle with `:Gitsigns toggle_signs`
-  numhl = true,       -- Toggle with `:Gitsigns toggle_numhl`
-  linehl = false,      -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff = false,   -- Toggle with `:Gitsigns toggle_word_diff`
+  signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+  numhl = true,      -- Toggle with `:Gitsigns toggle_numhl`
+  linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
     interval = 1000,
     follow_files = true,
   },
   attach_to_untracked = true,
-  current_line_blame = false,   -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
-    virt_text = true,
-    virt_text_pos = "eol",     -- 'eol' | 'overlay' | 'right_align'
-    delay = 100,
+    virt_text         = true,
+    virt_text_pos     = "eol", -- 'eol' | 'overlay' | 'right_align'
+    delay             = 100,
     ignore_whitespace = false,
   },
   current_line_blame_formatter_opts = {
@@ -32,7 +32,7 @@ gitsigns.setup {
   },
   sign_priority = 6,
   update_debounce = 100,
-  status_formatter = nil,   -- Use default
+  status_formatter = nil, -- Use default
   max_file_length = 40000,
   preview_config = {
     -- Options passed to nvim_open_win
