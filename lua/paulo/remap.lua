9d0d6edb8e5dafa9c2a01ed0a4e3a852f92e1150
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-22 16:46:59 -0300

    feat(Debug): Included keymaps for debugging

--- lua/paulo/remap.lua
+++ lua/paulo/remap.lua
@@ -126,3 +126,36 @@ vim.keymap.set('n', '<leader>tl', gs.toggle_linehl)
 
 -- SHOW TODOS
 vim.keymap.set('n', '<leader>std', ':TodoTelescope<CR>')
+
+-- DAP
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp',
+  function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+  require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+  require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
+
+-- NEOTEST
+vim.keymap.set('n', '<leader>tr',  ':Neotest run<CR>',                                         { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>tra', ':lua require("neotest").run.run(vim.fn.expand("%")) <CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ts',  ':Neotest summary<CR>',                                     { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>to',  ':Neotest output<CR>',                                      { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>top', ':Neotest output-panel<CR>',                                { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>trd', ':lua require("neotest").run.run({strategy = "dap"})<CR>',  { noremap = true, silent = true })
