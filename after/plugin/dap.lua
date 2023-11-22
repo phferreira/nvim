d61fa6ca599f9e8a27409e02b7cd0d23687704b9
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-22 16:55:00 -0300

    feat(Debug): Added configurations files for debbuging and debugging test

---
+++ after/plugin/dap.lua
@@ -0,0 +1,46 @@
+local dap = require('dap')
+
+dap.adapters.dart = {
+  type = 'executable',
+  command = 'dart',
+  args = {'debug_adapter'}
+}
+dap.adapters.flutter = {
+  type = 'executable',
+  command = 'flutter',
+  args = {'debug_adapter'}
+}
+dap.configurations.dart = {
+  {
+    type = "dart",
+    request = "launch",
+    name = "Launch dart",
+    dartSdkPath = "/home/paulo/.development/flutter/bin/cache/dart-sdk/bin", -- ensure this is correct
+    flutterSdkPath = "/home/paulo/.development/flutter/",                  -- ensure this is correct
+    program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
+    cwd = "${workspaceFolder}",
+  },
+  {
+    type = "flutter",
+    request = "launch",
+    name = "Launch flutter",
+    dartSdkPath = "/home/paulo/.development/flutter/bin/cache/dart-sdk/bin", -- ensure this is correct
+    flutterSdkPath = "/home/paulo/.development/flutter/",             -- ensure this is correct
+    program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
+    cwd = "${workspaceFolder}",
+    toolArgs = {"-d", "chrome"}
+  }
+}
+
+local dapui = require("dapui")
+dapui.setup();
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+  dapui.close()
+end
---
+++ after/plugin/neotest.lua
@@ -0,0 +1,13 @@
+require("neotest").setup({
+  adapters = {
+    require('neotest-dart') {
+      command = 'flutter',        -- Command being used to run tests. Defaults to `flutter`
+      -- Change it to `fvm flutter` if using FVM
+      -- change it to `dart` for Dart only tests
+      use_lsp = true,        -- When set Flutter outline information is used when constructing test name.
+      -- Useful when using custom test names with @isTest annotation
+      custom_test_method_names = {},
+    },
+  },
+})
+
