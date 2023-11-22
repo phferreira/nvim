d507cc63c4557c6ecebc424d09ed41bd9b77707b
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-22 18:37:06 -0300

    docs: Indentation and change " to '

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -3,35 +3,34 @@ local dap = require('dap')
 dap.adapters.dart = {
   type = 'executable',
   command = 'dart',
-  args = {'debug_adapter'}
+  args = { 'debug_adapter' }
 }
 dap.adapters.flutter = {
   type = 'executable',
   command = 'flutter',
-  args = {'debug_adapter'}
+  args = { 'debug_adapter' }
 }
 dap.configurations.dart = {
   {
-    type = "dart",
-    request = "launch",
-    name = "Launch dart",
-    dartSdkPath = "/home/paulo/.development/flutter/bin/cache/dart-sdk/bin", -- ensure this is correct
-    flutterSdkPath = "/home/paulo/.development/flutter/",                  -- ensure this is correct
-    program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
-    cwd = "${workspaceFolder}",
+    type = 'dart',
+    request = 'launch',
+    name = 'Launch dart',
+    dartSdkPath = '/home/paulo/.development/flutter/bin/cache/dart-sdk/bin', -- ensure this is correct
+    flutterSdkPath = '/home/paulo/.development/flutter/',                    -- ensure this is correct
+    program = '${workspaceFolder}/lib/main.dart',                            -- ensure this is correct
+    cwd = '${workspaceFolder}',
     toolArgs = {
       '--dart-define-from-file', './keys.json'
     }
   },
   {
-    type = "flutter",
-    request = "launch",
-    dartSdkPath = "/home/paulo/.development/flutter/bin/cache/dart-sdk/bin", -- ensure this is correct
-    flutterSdkPath = "/home/paulo/.development/flutter/",             -- ensure this is correct
-    program = "${workspaceFolder}/lib/main.dart",     -- ensure this is correct
-    cwd = "${workspaceFolder}",
-    toolArgs = {"-d", "chrome"}
+    type = 'flutter',
+    request = 'launch',
     name = 'Launch Flutter Linux',
+    dartSdkPath = '/home/paulo/.development/flutter/bin/cache/dart-sdk/bin', -- ensure this is correct
+    flutterSdkPath = '/home/paulo/.development/flutter/',                    -- ensure this is correct
+    program = '${workspaceFolder}/lib/main.dart',                            -- ensure this is correct
+    cwd = '${workspaceFolder}',
     toolArgs = {
       '-d', 'linux',
       '--dart-define-from-file', './keys.json'
@@ -52,15 +51,15 @@ dap.configurations.dart = {
   }
 }
 
-local dapui = require("dapui")
+local dapui = require('dapui')
 dapui.setup();
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
+dap.listeners.after.event_initialized['dapui_config'] = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
+dap.listeners.before.event_terminated['dapui_config'] = function()
   dapui.close()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
+dap.listeners.before.event_exited['dapui_config'] = function()
   dapui.close()
 end
