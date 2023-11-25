f7bf2735134ee21537910b9837016675e648254b
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-25 02:04:07 -0300

    feat(Tail): Implement Tail function for logs

--- lua/paulo/utils.lua
+++ lua/paulo/utils.lua
@@ -18,3 +18,50 @@ function ToggleDiagnostics()
   end
 end
 
+local vim = vim
+local loop = vim.loop
+
+local watchers = {}
+
+local function on_change(bufnr, filepath)
+  vim.print(bufnr)
+  vim.api.nvim_buf_call(bufnr, function()
+    vim.cmd('checktime')
+    vim.cmd('normal Gzz')
+  end)
+end
+
+local function watch_file(bufnr, filepath)
+  local w = loop.new_fs_poll()
+  w:start(filepath, 1000, vim.schedule_wrap(function()
+    on_change(bufnr, filepath)
+  end))
+  watchers[bufnr] = w
+end
+
+local function stop_watch(bufnr)
+  local w = watchers[bufnr]
+  if w then
+    w:stop()
+    watchers[bufnr] = nil
+  end
+end
+
+function DoTail()
+  local current_buffer_number = vim.fn.bufnr('%')
+
+  if vim.b[current_buffer_number].isTail == nil then
+    vim.b[current_buffer_number].isTail = false
+  end
+
+  if vim.b[current_buffer_number].isTail then
+    stop_watch(current_buffer_number)
+    vim.b[current_buffer_number].isTail = false
+  else
+    vim.cmd('setlocal syntax=logtalk')
+    watch_file(current_buffer_number, vim.fn.expand('%:p'))
+    vim.b[current_buffer_number].isTail = true
+  end
+end
+
+vim.cmd('command! Tail execute \'lua DoTail()\'')
