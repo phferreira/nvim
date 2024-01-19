86396260e141113440c3ce4b373c1c27c86eff16
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-01-19 19:43:02 -0300

    fix(Buffer): On change buffers, the old buffer was deleted
    - Starts to only save view, load view is doing by nvim

--- lua/paulo/utils.lua
+++ lua/paulo/utils.lua
@@ -1,4 +1,4 @@
- function DoPrettyXML()
+function DoPrettyXML()
   vim.cmd('set ft=xml')
   vim.cmd(':0')
   vim.cmd(':silent! %s/> *</>\r</g')
@@ -71,9 +71,4 @@ vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
   pattern = { "*.*" },
   desc = "save view (folds), when closing file",
   command = "mkview",
-})
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  pattern = { "*.*" },
-  desc = "load view (folds), when opening file",
-  command = "silent! loadview"
 })
