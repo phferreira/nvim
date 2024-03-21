7c8eae69e5107e1c0a147dc3372446c3c8ebeee4
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-03-21 20:00:47 -0300

    fix(Dependency): Fix dependency of neotest plugin

--- lua/paulo/packer.lua
+++ lua/paulo/packer.lua
@@ -24,6 +24,7 @@ return require('packer').startup(function(use)
   use {
     "nvim-neotest/neotest",
     requires = {
+      'nvim-neotest/nvim-nio',
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       'sidlatau/neotest-dart',
