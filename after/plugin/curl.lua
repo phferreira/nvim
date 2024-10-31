34222592020de56467092af20d77d293126e2aba
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2024-10-31 13:37:36 -0300

    feat(Curl Mappings): Add open Curl summary key mapping

--- after/plugin/curl.lua
+++ after/plugin/curl.lua
@@ -1,6 +1,12 @@
 local curl = require("curl")
 curl.setup({
+  -- Table of strings to specify default headers to be included in each request, i.e. "-i"
+  default_flags = {},
   mappings = {
     execute_curl = "<M-CR>"
   }
 })
+
+vim.keymap.set("n", "<leader>cc", function()
+  curl.open_curl_tab()
+end, { desc = "Open a curl tab scoped to the current working directory" })
