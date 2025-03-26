2eac1f005d489ef61691840414599763735afdcb
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2025-03-26 08:08:44 -0300

    feat(Dap Configuration): Remove Dap configuration
    - This was configured in flutter-tools

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,57 +1,5 @@
 local dap = require('dap')
 
-dap.adapters.dart = {
-  type = 'executable',
-  command = 'dart',
-  args = { 'debug_adapter' }
-}
-dap.adapters.flutter = {
-  type = 'executable',
-  command = 'flutter',
-  args = { 'debug_adapter' }
-}
-dap.configurations.dart = {
-  {
-    type = 'dart',
-    request = 'launch',
-    name = 'Launch dart',
-    dartSdkPath = '/home/paulo/.development/flutter/bin/cache/dart-sdk/bin', -- ensure this is correct
-    flutterSdkPath = '/home/paulo/.development/flutter/',                    -- ensure this is correct
-    program = '${workspaceFolder}/lib/main.dart',                            -- ensure this is correct
-    cwd = '${workspaceFolder}',
-    toolArgs = {
-      'bin/main.dart',
-      '--dart-define-from-file', '.keys.json'
-    }
-  },
-  {
-    type = 'flutter',
-    request = 'launch',
-    name = 'Launch Flutter Linux',
-    dartSdkPath = '/home/paulo/.development/flutter/bin/cache/dart-sdk/bin', -- ensure this is correct
-    flutterSdkPath = '/home/paulo/.development/flutter/',                    -- ensure this is correct
-    program = '${workspaceFolder}/lib/main.dart',                            -- ensure this is correct
-    cwd = '${workspaceFolder}',
-    toolArgs = {
-      '-d', 'linux',
-      '--dart-define-from-file', '.keys.json'
-    }
-  },
-  {
-    type = 'flutter',
-    request = 'launch',
-    name = 'Launch Flutter Chrome',
-    dartSdkPath = '/home/paulo/.development/flutter/bin/cache/dart-sdk/bin', -- ensure this is correct
-    flutterSdkPath = '/home/paulo/.development/flutter/',                    -- ensure this is correct
-    program = '${workspaceFolder}/lib/main.dart',                            -- ensure this is correct
-    cwd = '${workspaceFolder}',
-    toolArgs = {
-      '-d', 'chrome',
-      '--dart-define-from-file', '.keys.json'
-    }
-  }
-}
-
 local dapui = require('dapui')
 dapui.setup();
 
