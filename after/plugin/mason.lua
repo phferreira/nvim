f88fab471b0fb77c4b0456e64df696d3bf6aefe4
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2025-11-01 19:13:57 -0300

    refactor(Separated configs): create file for mason and null-ls
    - separated from lsp

---
+++ after/plugin/mason.lua
@@ -0,0 +1,28 @@
+-- Mason
+local ok_mason, mason = pcall(require, "mason")
+if not ok_mason then
+  vim.notify("Mason não encontrado")
+  return
+end
+
+mason.setup()
+
+-- Mason-LSPConfig
+local ok_mason_lsp, mason_lspconfig = pcall(require, "mason-lspconfig")
+if not ok_mason_lsp then
+  vim.notify("Mason-LSPConfig não encontrado")
+  return
+end
+
+mason_lspconfig.setup({
+  ensure_installed = {
+    "lua_ls",
+    "pyright",
+    "rust_analyzer",
+    "jsonls",
+    "yamlls",
+    "dockerls",
+    "ts_ls",
+    "jdtls",
+  },
+})
---
+++ after/plugin/null-ls.lua
@@ -0,0 +1,47 @@
+local ok, null_ls = pcall(require, "null-ls")
+if not ok then
+  vim.notify("null_ls não encontrado")
+  return
+end
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
+  sources = {
+    -- Python
+    formatting.black.with({ extra_args = { "--fast" } }),
+    diagnostics.flake8,
+
+    -- JS / TS
+    formatting.prettier.with({
+      filetypes = { "javascript", "typescript", "css", "html", "json" },
+    }),
+
+    -- Shell
+    formatting.shfmt,
+    -- diagnostics.shellcheck,
+
+    -- Gerais
+    code_actions.gitsigns,
+  },
+  -- Formatação automática ao salvar
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      -- Limpa autocmds existentes nesse grupo para este buffer
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+
+      -- Cria autocmd para formatar ao salvar
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr, async = false })
+        end,
+      })
+    end
+  end,
+})
