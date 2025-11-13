66d944ad37281a2c4cfda9f11d7fcd885e2c5511
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2025-11-13 18:18:24 -0300

    feat(Package Validate): Included validation for flutter-tools package

--- after/plugin/flutter_tools.lua
+++ after/plugin/flutter_tools.lua
@@ -1,4 +1,10 @@
-require("flutter-tools").setup {
+local ok_ft, flutter_tools = pcall(require, "flutter-tools")
+if not ok_ft then
+  vim.notify("flutter-tools.nvim não encontrado!", vim.log.levels.WARN)
+  return
+end
+
+flutter_tools.setup {
   fvm = true,
   debugger = {
     enabled = true,
@@ -13,6 +19,6 @@ require("flutter-tools").setup {
       dart = {
         lineLength = 100
       }
-    }
+    },
   }
 }
