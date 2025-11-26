8288e434c80d0464238e0048c66b7848a22acd93
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2025-11-26 17:05:22 -0300

    feat(vim-fugitive): Add utils functions for fugitive

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,2 +1,21 @@
-return { "tpope/vim-fugitive",
+return {
+  "tpope/vim-fugitive",
+  config = function()
+    local function GetCurrentBranch()
+      local handle = io.popen("git branch --show-current")
+      local output = handle:read("*a")
+      local branch = output:gsub("[\n\r]", " ")
+      handle.close()
+      return branch
+    end
+
+    function GReset()
+      vim.cmd("Git reset origin/" .. GetCurrentBranch() .. " --hard --recurse-submodules")
+    end
+
+    function GPull()
+      vim.cmd("Git pull origin " .. GetCurrentBranch() .. " --rebase --recurse-submodules")
+    end
+  end,
 }
+
